ce02f918aea0a3a2fde3b77fdefc52a40841cca4
milinches <ibrogenius5165@gmail.com>
2022-02-11 04:25:24 +0100

    made changes

--- accounts/models.py
+++ accounts/models.py
@@ -21,3 +21,17 @@ class Student(models.Model):
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     gender = models.CharField(choices=GENDER, max_length=2, blank=False)
     level = models.CharField(max_length=10, blank=False)
+
+
+class Lecturer(models.Model):
+    GENDER = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    email = models.EmailField(blank=False, null=False)
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    other_name = models.CharField(max_length=50, blank=True, null=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    gender = models.CharField(choices=GENDER, max_length=2, blank=False)
+    level = models.CharField(max_length=10, blank=False)
