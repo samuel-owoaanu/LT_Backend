9713c31eeac662d5106ebe0239485fb00ff7f53c
milinches <ibrogenius5165@gmail.com>
2022-02-13 16:19:36 +0100

    created a `custom account class`

--- lt_backend/settings.py
+++ lt_backend/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = [
     "127.0.0.1"
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
+
 
 # Application definition
 
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'timetable'
 ]
 
 MIDDLEWARE = [
